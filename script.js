139f78944455e4a2f21087b42cf628561038d597
Carlos Flores <carlosfernandoflores321@gmail.com>
2024-08-31 20:35:59 -0600

    debugging fecha o apareciendo en recordatorio

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ var year = now.getFullYear();
 var currentMonth = month;
 var currentDay = day;
 
+
 initCalendar();
 
 /* Metodos */
@@ -138,13 +139,62 @@ function addReminderLogic(form, bg_img) {
 }
 
 // Metodo para validar que haya escrito el titulo del reminder
-function validateForm() {
-    var input_area = document.getElementById("title-input");
+function validateForm(reminderTit) {
+    return reminderTit === "" ? false : true;
+}
 
-    if (input_area.value === "") {
-        return false;
+function onSaveClick(date) {
+
+}
+
+//metodo para crear el elemento div que sera el "reminder" como tal 
+function createReminderElement(reminderTit, sDate, sPriority) {
+    var reminder = document.createElement("div");
+    reminder.id = "reminderElement";
+
+    // crea un elemento input
+    var reminderTitleEl = document.createElement('input');
+    reminderTitleEl.id = "checkbox-id";
+
+    reminderTitleEl.type = 'checkbox';
+    reminderTitleEl.name = 'name';
+    reminderTitleEl.value = 'value';
+
+    // crea un label para el recordatorio
+    var label = document.createElement('label');
+    label.htmlFor = "checkbox-id";
+    label.id = "reminderTitle";
+
+    // obtiene el nombre del recordatorio
+    var labelValue = document.createTextNode(reminderTit);
+    labelValue.id = "reminderTitle"
+    label.appendChild(labelValue);
+
+    reminder.appendChild(reminderTitleEl);
+    reminder.appendChild(label);
+
+    // logica para la descripcion
+    var descriptionEl = document.getElementById("rem-description");
+
+    if (descriptionEl.value !== "") {
+        var remDesc = document.createElement('p');
+        remDesc.id = "reminderDesc";
+        remDesc.innerHTML = descriptionEl.value;
+        reminder.appendChild(remDesc);
     }
-    return true;
+
+    //logica para la fecha 
+    if (sDate != null) {
+        var remDate = document.createElement('p');
+        remDate.id = "reminderDate";
+        remDate.innerHTML  = sDate;
+        reminder.appendChild(remDate);
+    }
+
+    //logica para el nivel de prioridad
+
+
+    document.getElementById("items").appendChild(reminder);
 }
 
 /* Eventos de los botones */
@@ -222,45 +272,56 @@ document.getElementById("cancel-btn").addEventListener('click', function () {
     setTimeout(setFormInvisible, 500);
 });
 
-document.getElementById("save-btn").addEventListener('click', function () {
-    try {
-
-        if (validateForm()) {
-            alert("Hay algo escrito en el titulo!");
-            //TODO: Logica para agregar el recordatorio
-        }
-        else {
-            alert("Acuerdate de no dejar vacio el campo del titulo!");
-        }
-    } catch (error) {
-        alert("Ocurrio un error!");
-        console.log("Error: ", error.message);
-    }
-});
-
 document.addEventListener('DOMContentLoaded', function () {
     var dayElements = document.querySelectorAll('.item_day');
+    var priorities = document.querySelectorAll('.priority-btn');
+
+    var selPriority = null;
+    var selectedDate = null;
+
+    // logica para seleccionar la fecha 
     dayElements.forEach(function (dayElement) {
         dayElement.addEventListener('click', function () {
             var day = parseInt(this.textContent);
             console.log("El day es de tipo: " + typeof (day));
-            var selectedDate = getSelectedDate(day);
+            selectedDate = getSelectedDate(day);
 
             // TODO: Logica para utilizar la fecha seleccionada
-            alert("La fecha seleccionada: " + selectedDate);
+            //alert("La fecha seleccionada: " + selectedDate.toDateString());
         });
+    });
+
+    // logica para seleccionar el nivel de prioridad del recordatorio
+    priorities.forEach(function (priorityElement) {
+        priorityElement.addEventListener('click', function () {
+            selPriority = this.textContent;
+            selPriority.trim();
+            //alert("La prioridad seleccionada es: " + selPriority);
+        });
+    });
+
+    // logica para guardar el recordatorio
+    document.getElementById("save-btn").addEventListener('click', function () {
+        try {
+
+            var input_area = document.getElementById("title-input");
+            var reminderTit = input_area.value;
+
+            if (validateForm(reminderTit)) {
+                //TODO: Logica para agregar el recordatorio
+                alert("La fecha seleccionada: " + selectedDate.toDateString());
+                createReminderElement(reminderTit, selectedDate, selPriority);
+            }
+            else {
+                alert("Acuerdate de no dejar vacio el campo del titulo!");
+            }
+        } catch (error) {
+            alert("Ocurrio un error!");
+            console.log("Error: ", error.message);
+        }
     });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var priorities = document.querySelectorAll('.priority-btn');
-    priorities.forEach(function (priorityElement) {
-        priorityElement.addEventListener('click', function () {
-            var selPriority = this.textContent;
-            selPriority.trim();
-            alert("La prioridad seleccionada es: " + selPriority);
-        });
-    });
-});
+
 
 
